3ac1040144652f184412722dc0786f544f617603
Tailus Dev <4932abcd@gmail.com>
2024-02-03 14:03:10 +0330

    project get stated and base components were added to the app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+export default function App() {
+  return <div className="App">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+  </div>;
+}
 
-function App() {
+function Logo() {
+  return <h1>Far Away</h1>;
+}
+
+function Form() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="add-form">
+      <h3>What do you need for your trip ? </h3>
     </div>
   );
 }
 
-export default App;
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <en>you have X items on your list, and you already packed X (X%)</en>
+    </footer>
+  );
+}
